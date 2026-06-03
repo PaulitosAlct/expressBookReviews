131835ced4117235165cbbc44d0e33d484959dd0
PaulitosAlct <palmeida1988@gmail.com>
2026-06-03 00:20:56 -0400

    task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,20 +18,41 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    // Retrieve the ISBN parameter from the request URL and send the corresponding book details
+    const isbn = req.params.isbn;
+    res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    const booksByAuthor = [];
+    for (const bookId in books) {
+        if (books[bookId].author === author) {
+            booksByAuthor.push(books[bookId]);
+        }
+    }
+    if (booksByAuthor.length > 0) {
+        return res.json(booksByAuthor);
+    } else {
+        return res.status(404).json({message: "No books found by Author"});
+    }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    const booksByTitle = [];
+    for (const bookId in books) {
+        if (books[bookId].title === title) {
+            booksByTitle.push(books[bookId]);
+        }
+    }
+    if (booksByTitle.length > 0) {
+        return res.json(booksByTitle);
+    } else {
+        return res.status(404).json({message: "No books found by title"})
+    }
 });
 
 //  Get book review
